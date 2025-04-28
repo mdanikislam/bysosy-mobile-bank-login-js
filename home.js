179b4af025978abd67e78cda2d3cec56532d4f85
mdanikislam <anikislam487@gmail.com>
2025-04-29 01:25:36 +0600

    cashout feature update

--- home.js
+++ home.js
@@ -1,21 +1,44 @@
-document.getElementById('add-money').addEventListener('click',function(event){
+// for add money
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    console.log('clicked');
 
-    const inputBalance = document.getElementById('input-balance').value;
+    const input_Balance = document.getElementById("input-balance");
+    const inputBalance = input_Balance.value;
     const inputBalanceNum = parseFloat(inputBalance);
-    const inputPin = document.getElementById('input-balance-pin').value;
+    const input_Pin = document.getElementById("input-balance-pin");
+    const inputPin = input_Pin.value;
 
-    if(inputPin ==='1234'){
-        const mainBalance = document.getElementById('main-balance').innerText;
-        const balance = parseFloat(mainBalance);
-        const newBalance = balance + inputBalanceNum;
-        document.getElementById('main-balance').innerText = newBalance;
-        
-        
+    if (inputPin === "1234") {
+      const mainBalance = document.getElementById("main-balance").innerText;
+      const balance = parseFloat(mainBalance);
+      const newBalance = balance + inputBalanceNum;
+      document.getElementById("main-balance").innerText = newBalance;
+      input_Balance.value = "";
+      input_Pin.value = "";
+    } else {
+      alert("Failed, Try again");
     }
-    else{
-        alert ('Failed, Try again')
-    }
-    
-})
+  });
+
+// for cashout money
+document.getElementById("withdraw").addEventListener("click", function (event) {
+  event.preventDefault();
+  const input_Balance = document.getElementById("input-balance");
+  const inputBalance = input_Balance.value;
+  const inputBalanceNum = parseFloat(inputBalance);
+  const input_Pin = document.getElementById("input-balance-pin");
+  const inputPin = input_Pin.value;
+
+  if (inputPin === "1234") {
+    const mainBalance = document.getElementById("main-balance").innerText;
+    const balance = parseFloat(mainBalance);
+    const newBalance = balance - inputBalanceNum;
+    document.getElementById("main-balance").innerText = newBalance;
+    input_Balance.value = "";
+    input_Pin.value = "";
+  } else {
+    alert("Failed, Try again");
+  }
+});
